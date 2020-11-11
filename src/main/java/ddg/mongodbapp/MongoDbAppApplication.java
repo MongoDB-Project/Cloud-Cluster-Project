52b1c3d49d965146b6ee9bae16d4589f8fe8e463
dp4295 <deep@pdx.edu>
2020-11-10 23:15:54 -0800

    added project files

---
+++ src/main/java/ddg/mongodbapp/MongoDbAppApplication.java
@@ -0,0 +1,15 @@
+package ddg.mongodbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbAppApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/ddg/mongodbapp/MongoDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ddg.mongodbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
